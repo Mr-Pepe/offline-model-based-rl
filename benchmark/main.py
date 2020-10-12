c8d4c1fd7769c3eb129886833f24a04123877aff
Felipe Peter <mr-peipei@web.de>
2020-10-12 15:49:41 +0200

    Run half cheetah v2

--- benchmark/main.py
+++ benchmark/main.py
@@ -7,7 +7,7 @@ from benchmark.sac.sac import sac
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='MountainCarContinuous-v0')
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
     parser.add_argument('--hid', type=int, default=256)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
--- benchmark/test/test_sac_cpu.py
+++ benchmark/test/test_sac_cpu.py
@@ -9,7 +9,7 @@ from benchmark.utils.run_utils import setup_logger_kwargs
 logger_kwargs = setup_logger_kwargs('test_sac_cpu')
 
 def test_sac_cpu():
-    sac(lambda: gym.make('MountainCarContinuous-v0'), actor_critic=MLPActorCritic,
+    sac(lambda: gym.make('HalfCheetah-v2'), actor_critic=MLPActorCritic,
         ac_kwargs=dict(hidden_sizes=[3, 3]), epochs=1,
         logger_kwargs=logger_kwargs, device='cpu')
 
