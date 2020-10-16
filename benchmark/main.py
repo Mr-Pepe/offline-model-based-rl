acb259b4af67c3bd5da3eab61e7322376029a7bd
Felipe Peter <mr-peipei@web.de>
2020-10-16 15:48:41 +0200

    Add MBPO arguments to main

--- benchmark/main.py
+++ benchmark/main.py
@@ -13,9 +13,12 @@ if __name__ == '__main__':
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--exp_name', type=str, default='sac')
-    parser.add_argument('--device', type=str, default='cpu')
+    parser.add_argument('--epochs', type=int, default=400)
+    parser.add_argument('--steps_per_epoch', type=int, default=1000)
+    parser.add_argument('--model_rollouts', type=int, default=400)
+    parser.add_argument('--agent_updates', type=int, default=40)
+    parser.add_argument('--exp_name', type=str, default='mbpo')
+    parser.add_argument('--device', type=str, default='cuda')
     args = parser.parse_args()
 
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
@@ -28,4 +31,6 @@ if __name__ == '__main__':
     train(lambda: gym.make(args.env),
           sac_kwargs=dict(hidden_sizes=[args.hid]*args.l, gamma=args.gamma),
           seed=args.seed, epochs=args.epochs,
-          logger_kwargs=logger_kwargs, device=device)
+          logger_kwargs=logger_kwargs, device=device, init_steps=10000,
+          use_model=True, steps_per_epoch=args.steps_per_epoch, 
+          agent_updates=args.agent_updates, model_rollouts=args.model_rollouts)
