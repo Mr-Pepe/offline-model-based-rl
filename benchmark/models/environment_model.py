Fix calculation of residual

benchmark/models/environment_model.py
@@ -42,33 +42,44 @@ class EnvironmentModel(nn.Module):
                                     requires_grad=True)
 
     def predict_mean_and_logvar(self, x):
-        x = self.check_device_and_shape(x)
+        device = next(self.parameters()).device
+        x = self.check_device_and_shape(x, device)
 
         out = self.layers(x)
         mean, logvar = out[:, self.out_dim:], out[:, :self.out_dim]
         logvar = self.max_logvar - softplus(self.max_logvar - logvar)
         logvar = self.min_logvar + softplus(logvar - self.min_logvar)
 
-        return mean, logvar, self.max_logvar, self.min_logvar
+        # Only learn a residual
+        return mean - torch.cat((x[:, :self.obs_dim],
+                                 torch.zeros((x.shape[0], 1),
+                                             device=device)),
+                                dim=1),\
+            logvar, \
+            self.max_logvar, \
+            self.min_logvar
 
     def forward(self, x):
         device = next(self.parameters()).device
         x = self.check_device_and_shape(x, device)
 
         if self.type == 'deterministic':
+            # Model only returns the residual
             out = self.layers(x)
+            out = out + torch.cat((x[:, :self.obs_dim],
+                                   torch.zeros((x.shape[0], 1), device=device)),
+                                  dim=1)
 
         else:
+            # Residual and current observation are already sumed up inside
+            # the function
             mean, logvar, _, _ = self.predict_mean_and_logvar(x)
 
             std = torch.exp(0.5*logvar)
             out = torch.normal(mean, std)
 
         # TODO: Transform angular input to [sin(x), cos(x)]
-        # Only learn a residual of the state
-        return out + torch.cat((x[:, :self.obs_dim],
-                                torch.zeros((x.shape[0], 1), device=device)),
-                               dim=1)
+        return out
 
     def get_prediction(self, x):
         return self.forward(x).cpu().detach().numpy()

benchmark/utils/loss_functions.py
@@ -11,12 +11,11 @@ def probabilistic_loss(x, y, model, only_mse=False):
     inv_var = torch.exp(-logvar)
 
     if only_mse:
-        std = torch.exp(0.5*logvar)
-        y_pred = torch.normal(mean, std)
-        return torch.square(y - y_pred).mean()
+        return torch.square(mean - y).mean()
     else:
-        mse_loss = (torch.square(
-            mean - y) * inv_var).mean()
+        mse_loss = (torch.square(mean - y) * inv_var).mean()
         var_loss = logvar.mean()
         var_bound_loss = 0.01*max_logvar.sum() - 0.01*min_logvar.sum()
+        # print("{:.3f}, {:.3f}, {:.3f}".format(
+        #     mse_loss, var_loss, var_bound_loss))
         return mse_loss + var_loss + var_bound_loss