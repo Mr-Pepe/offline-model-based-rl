2c346b4ce3f235f051bff86b9d2dc06fba4bffa5
Felipe Peter <mr-peipei@web.de>
2020-10-26 14:19:38 +0100

    Specify all arguments

--- benchmark/vanilla_sac.sh
+++ benchmark/vanilla_sac.sh
@@ -2,6 +2,8 @@ python main.py \
     --env HalfCheetah-v2 \
     --hid 200 \
     --l 4 \
+    --gamma 0.99 \
+    --seed 0 \
     --epochs 400 \
     --steps_per_epoch 1000 \
     --init_steps 1000 \
