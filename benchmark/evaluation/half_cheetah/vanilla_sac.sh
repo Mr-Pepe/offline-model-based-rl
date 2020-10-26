d8b39149307becc54d24cdac5c402a5f1869110d
Felipe Peter <mr-peipei@web.de>
2020-10-26 14:19:38 +0100

    Generate configurations for MBPO

---
+++ benchmark/evaluation/half_cheetah/vanilla_sac.sh
@@ -0,0 +1,16 @@
+# Vanilla SAC without any specialties
+
+python main.py \
+    --env HalfCheetah-v2 \
+    --hid 200 \
+    --l 4 \
+    --gamma 0.99 \
+    --seed 0 \
+    --epochs 400 \
+    --steps_per_epoch 1000 \
+    --init_steps 1000 \
+    --use_model False \
+    --agent_updates 1 \
+    --num_test_episodes 10 \
+    --exp_name vanilla_sac \
+    --device cuda
--- benchmark/evaluation/half_cheetah/vanilla_sac_multi_update.sh
+++ benchmark/evaluation/half_cheetah/vanilla_sac_multi_update.sh
@@ -1,3 +1,6 @@
+# SAC with multiple agent updates per environment step to compare to the
+# multiple updates every step of MBPO
+
 python main.py \
     --env HalfCheetah-v2 \
     --hid 200 \
@@ -8,7 +11,7 @@ python main.py \
     --steps_per_epoch 1000 \
     --init_steps 1000 \
     --use_model False \
-    --agent_updates 1 \
+    --agent_updates 40 \
     --num_test_episodes 10 \
-    --exp_name vanilla_sac \
+    --exp_name vanilla_sac_multi_update \
     --device cuda
