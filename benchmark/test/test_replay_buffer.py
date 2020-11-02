8b9262f522907562d3f42be12469abfbcf06a16e
Felipe Peter <mr-peipei@web.de>
2020-11-02 13:08:16 +0100

    Replay buffer returns batch with balanced done signal

--- benchmark/test/test_replay_buffer.py
+++ benchmark/test/test_replay_buffer.py
@@ -27,3 +27,33 @@ def test_buffer_returns_percentage_of_terminal_states():
 
     np.testing.assert_almost_equal(
         buffer.get_terminal_ratio(), dones.sum()/dones.size)
+
+
+def test_buffer_returns_whether_it_contains_a_done_state():
+    buffer = ReplayBuffer(1, 1, 1000)
+
+    assert not buffer.has_terminal_state()
+
+    for step in range(1000):
+        buffer.store(0, 0, 0, 0, False)
+
+    assert not buffer.has_terminal_state()
+
+    for step in range(1000):
+        buffer.store(0, 0, 0, 0, True)
+
+    assert buffer.has_terminal_state()
+
+
+def test_buffer_returns_batch_with_balanced_terminal_signal():
+    buffer = ReplayBuffer(1, 1, 1000)
+
+    # Next check that batch is balanced if buffer is unbalanced
+    for step in range(1001):
+        buffer.store(0, 0, 0, 0, step % 4 == 0)
+
+    assert buffer.get_terminal_ratio() < 0.5
+
+    batch = buffer.sample_balanced_terminal_batch()
+
+    assert sum(batch['done']) == 0.5 * len(batch['done'])
--- benchmark/utils/replay_buffer.py
+++ benchmark/utils/replay_buffer.py
@@ -2,6 +2,7 @@ import torch
 from benchmark.utils.combined_shape import combined_shape
 import numpy as np
 
+
 class ReplayBuffer:
     """
     A simple FIFO experience replay buffer for SAC agents.
@@ -9,9 +10,12 @@ class ReplayBuffer:
     """
 
     def __init__(self, obs_dim, act_dim, size):
-        self.obs_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
-        self.obs2_buf = np.zeros(combined_shape(size, obs_dim), dtype=np.float32)
-        self.act_buf = np.zeros(combined_shape(size, act_dim), dtype=np.float32)
+        self.obs_buf = np.zeros(combined_shape(
+            size, obs_dim), dtype=np.float32)
+        self.obs2_buf = np.zeros(combined_shape(
+            size, obs_dim), dtype=np.float32)
+        self.act_buf = np.zeros(combined_shape(
+            size, act_dim), dtype=np.float32)
         self.rew_buf = np.zeros(size, dtype=np.float32)
         self.done_buf = np.zeros(size, dtype=np.float32)
         self.ptr, self.size, self.max_size = 0, 0, size
@@ -37,10 +41,12 @@ class ReplayBuffer:
                      act=self.act_buf[idxs],
                      rew=self.rew_buf[idxs],
                      done=self.done_buf[idxs])
-        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
+        return {k: torch.as_tensor(v, dtype=torch.float32)
+                for k, v in batch.items()}
 
     def sample_train_batch(self, batch_size=32, val_split=0.2):
-        if self.split_at_size != self.size or self.split_with_val_split != val_split:
+        if self.split_at_size != self.size or \
+                self.split_with_val_split != val_split:
             self.split(val_split)
 
         idxs = np.random.choice(self.train_idxs, batch_size)
@@ -49,10 +55,12 @@ class ReplayBuffer:
                      act=self.act_buf[idxs],
                      rew=self.rew_buf[idxs],
                      done=self.done_buf[idxs])
-        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
+        return {k: torch.as_tensor(v, dtype=torch.float32)
+                for k, v in batch.items()}
 
     def sample_val_batch(self, batch_size=32, val_split=0.2):
-        if self.split_at_size != self.size or self.split_with_val_split != val_split:
+        if self.split_at_size != self.size or \
+                self.split_with_val_split != val_split:
             self.split(val_split)
 
         idxs = np.random.choice(self.val_idxs, batch_size)
@@ -61,14 +69,46 @@ class ReplayBuffer:
                      act=self.act_buf[idxs],
                      rew=self.rew_buf[idxs],
                      done=self.done_buf[idxs])
-        return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
+        return {k: torch.as_tensor(v, dtype=torch.float32)
+                for k, v in batch.items()}
 
     def split(self, val_split=0.2):
         self.split_at_size = self.size
         self.split_with_val_split = val_split
 
-        self.val_idxs = np.random.choice(np.arange(self.size), int(self.size*val_split), replace=False)
+        self.val_idxs = np.random.choice(
+            np.arange(self.size), int(self.size*val_split), replace=False)
         self.train_idxs = np.setdiff1d(np.arange(self.size), self.val_idxs)
+        self.done_idx = np.argwhere(self.done_buf == True).reshape((-1))
+        self.not_done_idx = np.argwhere(self.done_buf == False).reshape((-1))
 
     def get_terminal_ratio(self):
         return self.done_buf.sum()/self.size
+
+    def has_terminal_state(self):
+        return self.done_buf.sum() > 0
+
+    def sample_balanced_terminal_batch(self, batch_size=32, val_split=0.2):
+        if not self.has_terminal_state():
+            raise Exception("Buffer has no terminal state.")
+
+        if self.split_at_size != self.size or \
+                self.split_with_val_split != val_split:
+            self.split(val_split)
+
+        done_idxs = np.random.choice(self.done_idx, batch_size//2)
+        not_done_idxs = np.random.choice(self.not_done_idx, batch_size//2)
+
+        batch = dict(obs=np.ravel((self.obs_buf[done_idxs],
+                                   self.obs_buf[not_done_idxs]), order='F'),
+                     obs2=np.ravel((self.obs2_buf[done_idxs],
+                                    self.obs2_buf[not_done_idxs]), order='F'),
+                     act=np.ravel((self.act_buf[done_idxs],
+                                   self.act_buf[not_done_idxs]), order='F'),
+                     rew=np.ravel((self.rew_buf[done_idxs],
+                                   self.rew_buf[not_done_idxs]), order='F'),
+                     done=np.ravel((self.done_buf[done_idxs],
+                                    self.done_buf[not_done_idxs]), order='F'))
+
+        return {k: torch.as_tensor(v, dtype=torch.float32)
+                for k, v in batch.items()}
