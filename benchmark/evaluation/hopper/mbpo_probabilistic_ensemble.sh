0786aaa91b5c70c7f5d40a9864b3670a9e5685ce
Felipe Peter <mr-peipei@web.de>
2020-11-02 15:21:36 +0100

    Hopper MBPO with termination function

--- benchmark/evaluation/hopper/mbpo_probabilistic_ensemble.sh
+++ benchmark/evaluation/hopper/mbpo_probabilistic_ensemble.sh
@@ -2,6 +2,7 @@
 
 python main.py \
     --env Hopper-v2 \
+    --term_fn hopper \
     --hid 200 \
     --l 4 \
     --gamma 0.99 \
@@ -24,5 +25,5 @@ python main.py \
     --model_patience 20 \
     --agent_updates 20 \
     --num_test_episodes 10 \
-    --exp_name hopper_mbpo_probabilistic_ensemble \
+    --exp_name hopper_mbpo_probabilistic_ensemble_use_term_fn \
     --device cuda
