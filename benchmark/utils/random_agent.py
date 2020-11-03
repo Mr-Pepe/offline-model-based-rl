d0ff5eb6688e9b9591bd74948c1759381ae48e04
Felipe Peter <mr-peipei@web.de>
2020-11-03 14:19:17 +0100

    Fix random agent

--- benchmark/utils/random_agent.py
+++ benchmark/utils/random_agent.py
@@ -2,10 +2,11 @@
 
 class RandomAgent():
     def __init__(self, env):
+        self.training = False
         self.act_space = env.action_space
 
     def eval(self):
         pass
 
-    def get_action(self):
-        return self.act_space.sample()
+    def get_action(self, obs=0):
+        return self.act_space.sample().reshape((1, -1))
