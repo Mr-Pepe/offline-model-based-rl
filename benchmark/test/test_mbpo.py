4d1ef7889e6298a3e79925eb248c8a12c428cd96
Felipe Peter <mr-peipei@web.de>
2020-11-03 14:58:08 +0100

    Test MBPO on hopper

--- benchmark/test/test_mbpo.py
+++ benchmark/test/test_mbpo.py
@@ -3,19 +3,21 @@ import torch
 from benchmark.train import train
 from benchmark.utils.run_utils import setup_logger_kwargs
 
-logger_kwargs = setup_logger_kwargs('test_mbpo')
-env = 'HalfCheetah-v2'
-
 
 def test_mbpo_with_single_deterministic_model_converges():
+    logger_kwargs = setup_logger_kwargs('test_mbpo')
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    n_epochs = 5
 
-    final_return = train(lambda: gym.make(env),
-                         epochs=n_epochs,
+    final_return = train(lambda: gym.make('Hopper-v2'),
+                         term_fn='hopper',
+                         steps_per_epoch=1000,
+                         random_steps=1000,
+                         epochs=20,
+                         use_model=True,
+                         model_rollouts=1,
                          logger_kwargs=logger_kwargs,
-                         device=device, use_model=True, steps_per_epoch=1200)
+                         device=device)
 
     assert final_return
 
-    assert final_return > 500
+    assert final_return > 200
