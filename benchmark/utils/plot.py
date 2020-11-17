1dbe2ae836b2b3383cb7bd1d1f439b6a8cb50c25
Felipe Peter <mr-peipei@web.de>
2020-11-17 11:22:07 +0100

    Fix legend in plots

--- benchmark/utils/plot.py
+++ benchmark/utils/plot.py
@@ -35,8 +35,8 @@ def plot_data(data, xaxis='Epoch', value="AverageEpRet", condition="Condition1",
         data = pd.concat(data, ignore_index=True)
     sns.set(style="darkgrid", font_scale=1.5)
     sns.lineplot(data=data, x=xaxis, y=value, hue=condition, ci='sd', **kwargs)
-    plt.legend(loc='upper center', ncol=6, handlelength=1,
-               mode="expand", borderaxespad=0., prop={'size': 13})
+    plt.legend(loc='best', ncol=1, handlelength=1,
+               borderaxespad=0., prop={'size': 13})
 
     xscale = np.max(np.asarray(data[xaxis])) > 5e3
     if xscale:
