b557d70a6aead49e2396b25c0571390a2f5fcbf5
Felipe Peter <mr-peipei@web.de>
2020-11-23 14:30:14 +0100

    Update half cheetah online SAC config

--- benchmark/evaluation/half_cheetah/vanilla_sac.sh
+++ benchmark/evaluation/half_cheetah/vanilla_sac.sh
@@ -1,19 +1,22 @@
-# Vanilla SAC without any specialties
-
 python main.py \
     --env HalfCheetah-v2 \
+    --seed 0 \
+    --epochs 400 \
+    --pretrain_epochs 0 \
+    --steps_per_epoch 1000 \
+    --init_steps 256 \
+    --random_steps 1000 \
+    --buffer_size 1000000 \
     --hid 200 \
     --l 4 \
     --gamma 0.99 \
-    --seed 0 \
-    --epochs 400 \
-    --steps_per_epoch 1000 \
-    --init_steps 1000 \
-    --random_steps 10000 \
+    --agent_updates_per_step 1 \
     --agent_batch_size 256 \
     --agent_lr 3e-4 \
     --use_model False \
-    --agent_updates 1 \
     --num_test_episodes 10 \
-    --exp_name half_cheetah_vanilla_sac \
-    --device cuda
+    --exp_name half_cheetah_sac_online \
+    --datestamp False \
+    --log_dir "" \
+    --device cpu \
+    --render False
