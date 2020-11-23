82426253c0fa2d1298762edf6f277b6b12a71cfa
Felipe Peter <mr-peipei@web.de>
2020-11-23 15:01:44 +0100

    Run half cheetah online MBPO on several seeds

--- benchmark/evaluation/half_cheetah/mbpo_online.sh
+++ benchmark/evaluation/half_cheetah/mbpo_online.sh
@@ -6,21 +6,30 @@ python main.py \
     --epochs 200 \
     --pretrain_epochs 0 \
     --steps_per_epoch 1000 \
-    --init_steps 256 \
+    --init_steps 2000 \
     --random_steps 1000 \
     --buffer_size 1000000 \
     --hid 200 \
     --l 4 \
     --gamma 0.99 \
-    --agent_updates_per_step 1 \
+    --agent_updates_per_step 40 \
     --agent_batch_size 256 \
     --agent_lr 3e-4 \
-    --use_model False \
+    --use_model True \
+    --model_type probabilistic \
+    --n_networks 7 \
+    --model_batch_size 256 \
+    --model_lr 1e-3 \
+    --model_val_split 0.2 \
+    --model_patience 20 \
+    --rollouts_per_step 400 \
+    --rollout_schedule 1 1 20 100 \
+    --train_model_every 250 \
     --num_test_episodes 10 \
-    --exp_name half_cheetah_sac_online \
+    --exp_name half_cheetah_mbpo_online \
     --datestamp False \
     --log_dir "" \
-    --device cpu \
+    --device cuda \
     --render False
 done
 exit 0
