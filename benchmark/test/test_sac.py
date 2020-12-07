975eb2fbae5deb86e4cb7d774e0b6a605cbebc80
Felipe Peter <mr-peipei@web.de>
2020-12-07 15:15:28 +0100

    Add test for offline SAC

--- benchmark/test/test_sac.py
+++ benchmark/test/test_sac.py
@@ -9,15 +9,13 @@ from benchmark.train import Trainer
 from benchmark.utils.run_utils import setup_logger_kwargs
 import d4rl  # noqa
 
-logger_kwargs = setup_logger_kwargs('test_sac')
-env = 'HalfCheetah-v2'
-
 
 @pytest.mark.fast
 def test_total_steps_must_be_enough_to_perform_at_least_one_update():
     init_steps = 150
     n_steps_per_epoch = 100
     n_epochs = 1
+    env = 'HalfCheetah-v2'
 
     with pytest.raises(ValueError):
         trainer = Trainer(env,
@@ -31,7 +29,9 @@ def test_total_steps_must_be_enough_to_perform_at_least_one_update():
 
 @pytest.mark.slow
 def test_sac_converges():
+    logger_kwargs = setup_logger_kwargs('test_sac')
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    env = 'HalfCheetah-v2'
 
     trainer = Trainer(env,
                       sac_kwargs=dict(hidden=[256, 256, 256, 256],
@@ -41,6 +41,34 @@ def test_sac_converges():
                       steps_per_epoch=4000,
                       num_test_episodes=10,
                       epochs=5,
+                      logger_kwargs=logger_kwargs,
+                      device=device,
+                      seed=0)
+
+    final_return, _ = trainer.train()
+
+    assert final_return[-1, -1] > 400
+
+
+@pytest.mark.slow
+def test_sac_offline():
+    logger_kwargs = setup_logger_kwargs('test_sac_offline')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    env = 'halfcheetah-random-v0'
+    n_samples = 900000
+
+    trainer = Trainer(env,
+                      sac_kwargs=dict(hidden=[200, 200, 200, 200],
+                                      batch_size=256),
+                      random_steps=0,
+                      agent_updates_per_step=1,
+                      render=True,
+                      init_steps=0,
+                      n_samples_from_dataset=n_samples,
+                      steps_per_epoch=1000,
+                      num_test_episodes=10,
+                      epochs=0,
+                      pretrain_epochs=20,
                       logger_kwargs=logger_kwargs,
                       device=device,
                       seed=0)
