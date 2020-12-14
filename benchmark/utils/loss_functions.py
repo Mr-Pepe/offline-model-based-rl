573e75cd8683df2f132c05af2c32922cd8a79e36
Felipe Peter <mr-peipei@web.de>
2020-12-14 10:39:35 +0100

    Improve debug logging in prob loss

--- benchmark/utils/loss_functions.py
+++ benchmark/utils/loss_functions.py
@@ -10,7 +10,7 @@ def deterministic_loss(x, y, model, i_network=-1):
         return torch.square(y_pred[i_network, :, :-1] - y.unsqueeze(0)).mean()
 
 
-def probabilistic_loss(x, y, model, i_network=-1, only_mse=False):
+def probabilistic_loss(x, y, model, i_network=-1, only_mse=False, debug=False):
     _, mean, logvar, max_logvar, min_logvar = model(x)
 
     if i_network > -1:
@@ -24,8 +24,22 @@ def probabilistic_loss(x, y, model, i_network=-1, only_mse=False):
     if only_mse:
         return torch.square(mean - y).mean()
     else:
-        mse_loss = (torch.square(mean - y) * inv_var).mean()
+        mse_loss = torch.square(mean - y)
+        mse_inv_var_loss = (mse_loss * inv_var).mean()
         var_loss = logvar.mean()
-        var_bound_loss = 0.01 * max_logvar.sum() - 0.01 * min_logvar.sum()
+        var_bound_loss = torch.clamp(
+            0.01 * max_logvar.sum() - 0.01 * min_logvar.sum(),
+            0,
+            1e10)
 
-        return mse_loss + var_loss + var_bound_loss
+        if debug:
+            print("MSE: {:.5f}, MSE + INV VAR: {:.5f} VAR: {:.5f}, BOUNDS: {:.5f}, MAX LOGVAR: {:.5f}, MIN LOGVAR: {:.5f}".format(
+                mse_loss.mean().item(),
+                mse_inv_var_loss.item(),
+                var_loss.item(),
+                var_bound_loss.item(),
+                max_logvar.mean().item(),
+                min_logvar.mean().item()
+            ), end='\r')
+
+        return mse_inv_var_loss + var_loss + var_bound_loss
