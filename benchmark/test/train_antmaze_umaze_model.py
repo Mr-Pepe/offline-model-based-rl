c6691c6407f3a1fd19288da79d86536eb387b416
Felipe Peter <mr-peipei@web.de>
2020-12-15 20:16:38 +0100

    Fix antmaze umaze model pretraining

--- benchmark/test/train_antmaze_umaze_model.py
+++ benchmark/test/train_antmaze_umaze_model.py
@@ -18,13 +18,14 @@ model = EnvironmentModel(obs_dim=obs_dim,
                          n_networks=7,
                          device=device,
                          pre_fn=get_preprocessing_function('antmaze-umaze-v0'),
-                         term_fn=get_postprocessing_function('antmaze-umaze-v0'),
+                         post_fn=get_postprocessing_function('antmaze-umaze-v0'),
                          )
 
 max_n_train_batches = -1
 
 model.train_to_convergence(buffer, debug=True, batch_size=128, lr=5e-4,
-                           max_n_train_batches=max_n_train_batches, patience=20)
+                           max_n_train_batches=max_n_train_batches, patience=30,
+                           val_split=0.1)
 model.cpu()
 
 torch.save(
