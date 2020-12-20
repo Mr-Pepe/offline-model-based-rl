0fea1a74773ca17a21f1a01f2c645ffdf18a1dae
Felipe Peter <mr-peipei@web.de>
2020-12-20 18:05:53 +0100

    Add reward function for antmaze medium

--- benchmark/utils/reward_functions.py
+++ benchmark/utils/reward_functions.py
@@ -1,4 +1,4 @@
-from benchmark.utils.mazes import ANTMAZE_UMAZE_GOAL_BLOCK
+from benchmark.utils.mazes import ANTMAZE_MEDIUM_GOAL_BLOCK, ANTMAZE_UMAZE_GOAL_BLOCK
 from benchmark.utils.envs import ENV_CATEGORIES
 
 
@@ -18,8 +18,25 @@ def antmaze_umaze_rew_fn(next_obs, **_):
     return rewards
 
 
+def antmaze_medium_rew_fn(next_obs, **_):
+    x = next_obs[:, :, 0]
+    y = next_obs[:, :, 1]
+
+    rewards = 1 * \
+        (ANTMAZE_MEDIUM_GOAL_BLOCK[0] <= x) * \
+        (ANTMAZE_MEDIUM_GOAL_BLOCK[1] > x) * \
+        (ANTMAZE_MEDIUM_GOAL_BLOCK[2] <= y) * \
+        (ANTMAZE_MEDIUM_GOAL_BLOCK[3] > y)
+    rewards = rewards.unsqueeze(-1)
+
+    rewards = rewards*100
+
+    return rewards
+
+
 reward_functions = {
     'antmaze_umaze': antmaze_umaze_rew_fn,
+    'antmaze_medium': antmaze_medium_rew_fn
 }
 
 
