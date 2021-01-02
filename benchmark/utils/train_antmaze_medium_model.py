8337b706bec72ad82b1bce8504d644ef26ad9862
Felipe Peter <mr-peipei@web.de>
2021-01-02 10:57:58 +0100

    Improve script for pretraining antmaze medium model

--- benchmark/utils/train_antmaze_medium_model.py
+++ benchmark/utils/train_antmaze_medium_model.py
@@ -1,3 +1,4 @@
+import argparse
 from benchmark.utils.postprocessing import get_postprocessing_function
 from benchmark.utils.preprocessing import get_preprocessing_function
 from benchmark.models.environment_model import EnvironmentModel
@@ -5,29 +6,48 @@ from benchmark.utils.load_dataset import load_dataset_from_env
 import gym
 import d4rl  # noqa
 import torch
+from benchmark.utils.str2bool import str2bool
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-env = gym.make('antmaze-medium-diverse-v0')
-buffer, obs_dim, act_dim = load_dataset_from_env(env,
-                                                 buffer_device=device)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--augment', type=str2bool, default=False)
+    parser.add_argument('--rew_amp', type=int, default=100)
+    args = parser.parse_args()
 
-model = EnvironmentModel(obs_dim=obs_dim,
-                         act_dim=act_dim,
-                         hidden=4*[512],
-                         type='probabilistic',
-                         n_networks=15,
-                         device=device,
-                         pre_fn=get_preprocessing_function('antmaze-medium-diverse-v0'),
-                         post_fn=get_postprocessing_function('antmaze-medium-diverse-v0'),
-                         )
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    env = gym.make('antmaze-medium_diverse-v0')
 
-max_n_train_batches = -1
+    if args.augment:
+        print("Load augmented dataset")
+        buffer = torch.load(
+            '/home/felipe/Projects/thesis-code/data/datasets/antmaze_medium_augmented.p')
+        obs_dim = env.observation_space.shape[0]
+        act_dim = env.action_space.shape[0]
+        save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium/augmented_model.pt'
+    else:
+        print("Load non-augmented dataset")
+        buffer, obs_dim, act_dim = load_dataset_from_env(env,
+                                                         buffer_device=device)
+        save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium/model.pt'
 
-model.train_to_convergence(buffer, debug=True, batch_size=128, lr=5e-4,
-                           max_n_train_batches=max_n_train_batches, patience=30,
-                           val_split=0.1)
-model.cpu()
+    buffer.rew_buf *= args.rew_amp
+    print("Amplified reward with factor {}".format(args.rew_amp))
 
-torch.save(
-    model,
-    '/home/felipe/Projects/thesis-code/data/models/antmaze_medium/model.pt')
+    model = EnvironmentModel(obs_dim=obs_dim,
+                             act_dim=act_dim,
+                             hidden=4*[512],
+                             type='probabilistic',
+                             n_networks=15,
+                             device=device,
+                             pre_fn=get_preprocessing_function(
+                                 'antmaze-medium_diverse-v0'),
+                             post_fn=get_postprocessing_function(
+                                 'antmaze-medium_diverse-v0'),
+                             )
+
+    model.train_to_convergence(buffer, debug=True, batch_size=128, lr=5e-4,
+                               max_n_train_batches=-1, patience=15,
+                               val_split=0.1)
+    model.cpu()
+
+    torch.save(model, save_path)
