86f898a76fe1d358acdf51d53883c73c49fad2c0
Felipe Peter <mr-peipei@web.de>
2021-01-04 15:18:04 +0100

    Don't run test env creation on import

--- benchmark/utils/envs.py
+++ benchmark/utils/envs.py
@@ -61,15 +61,15 @@ def antmaze_medium_diverse_test_env():
 
 
 TEST_ENV_MAPPING = {
-    'antmaze-umaze-v0': antmaze_umaze_test_env(),
-    'antmaze-umaze-diverse-v0': antmaze_umaze_diverse_test_env(),
-    'antmaze-medium-diverse-v0': antmaze_medium_diverse_test_env(),
+    'antmaze-umaze-v0': antmaze_umaze_test_env,
+    'antmaze-umaze-diverse-v0': antmaze_umaze_diverse_test_env,
+    'antmaze-medium-diverse-v0': antmaze_medium_diverse_test_env,
     'antmaze-medium-play-v0': None
 }
 
 
 def get_test_env(env_name):
     if env_name in TEST_ENV_MAPPING:
-        return TEST_ENV_MAPPING[env_name]
+        return TEST_ENV_MAPPING[env_name]()
     else:
         return gym.make(env_name)
