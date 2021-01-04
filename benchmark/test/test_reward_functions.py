c485f77fca4d5d862ec52913f43f454e44542463
Felipe Peter <mr-peipei@web.de>
2021-01-04 13:07:22 +0100

    Add custom reward function for antmaze umaze diverse

--- benchmark/test/test_reward_functions.py
+++ benchmark/test/test_reward_functions.py
@@ -1,4 +1,4 @@
-from benchmark.utils.reward_functions import get_reward_function
+from benchmark.utils.reward_functions import antmaze_umaze_diverse_rew_fn, get_reward_function
 import pytest
 import gym
 import d4rl  # noqa
@@ -22,5 +22,20 @@ def test_antmaze_umaze_reward_function():
     assert rewards.shape[1] == buffer.obs_buf.shape[0]
     assert rewards.shape[2] == 1
 
-    for r in rewards[0]:
-        assert r == 0 or r == 1
+
+@pytest.mark.medium
+def test_antmaze_umaze_diverse_reward_function():
+    env_name = 'antmaze-umaze-diverse-v0'
+    env = gym.make(env_name)
+    buffer, obs_dim, act_dim = load_dataset_from_env(env, 1000)
+
+    rew_fn = get_reward_function(env_name)
+
+    assert rew_fn is not None
+    assert rew_fn is antmaze_umaze_diverse_rew_fn
+
+    rewards = rew_fn(buffer.obs_buf.unsqueeze(0))
+
+    assert rewards.shape[0] == 1
+    assert rewards.shape[1] == buffer.obs_buf.shape[0]
+    assert rewards.shape[2] == 1
--- benchmark/utils/reward_functions.py
+++ benchmark/utils/reward_functions.py
@@ -1,5 +1,4 @@
-from benchmark.utils.mazes import ANTMAZE_ANT_RADIUS, ANTMAZE_UMAZE_GOAL
-from benchmark.utils.envs import ENV_CATEGORIES
+from benchmark.utils.mazes import ANTMAZE_ANT_RADIUS, ANTMAZE_UMAZE_DIVERSE_GOAL, ANTMAZE_UMAZE_GOAL
 import torch
 
 
@@ -16,15 +15,25 @@ def antmaze_umaze_rew_fn(next_obs, **_):
     return rewards
 
 
+def antmaze_umaze_diverse_rew_fn(next_obs, **_):
+    x = next_obs[:, :, 0]
+    y = next_obs[:, :, 1]
+
+    rewards = torch.sqrt(torch.square(x - ANTMAZE_UMAZE_DIVERSE_GOAL[0]) +
+                         torch.square(y - ANTMAZE_UMAZE_DIVERSE_GOAL[1])) < ANTMAZE_ANT_RADIUS
+    rewards = rewards.unsqueeze(-1)
+
+    rewards = rewards*100
+
+    return rewards
+
+
 def antmaze_medium_rew_fn(next_obs, **_):
     x = next_obs[:, :, 0]
     y = next_obs[:, :, 1]
 
-    rewards = 1 * \
-        (ANTMAZE_MEDIUM_GOAL_BLOCK[0] <= x) * \
-        (ANTMAZE_MEDIUM_GOAL_BLOCK[1] > x) * \
-        (ANTMAZE_MEDIUM_GOAL_BLOCK[2] <= y) * \
-        (ANTMAZE_MEDIUM_GOAL_BLOCK[3] > y)
+    rewards = torch.sqrt(torch.square(x - ANTMAZE_UMAZE_GOAL[0]) +
+                         torch.square(y - ANTMAZE_UMAZE_GOAL[1])) < ANTMAZE_ANT_RADIUS
     rewards = rewards.unsqueeze(-1)
 
     rewards = rewards*100
@@ -33,14 +42,14 @@ def antmaze_medium_rew_fn(next_obs, **_):
 
 
 reward_functions = {
-    'antmaze_umaze': antmaze_umaze_rew_fn,
+    'antmaze-umaze-v0': antmaze_umaze_rew_fn,
+    'antmaze-umaze-diverse-v0': antmaze_umaze_diverse_rew_fn,
     'antmaze_medium': antmaze_medium_rew_fn
 }
 
 
 def get_reward_function(env_name):
-    for fn_name in ENV_CATEGORIES:
-        if env_name in ENV_CATEGORIES[fn_name]:
-            return reward_functions[fn_name]
+    if env_name in reward_functions:
+        return reward_functions[env_name]
 
     return None
