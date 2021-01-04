83cf1db576aa87d86a5acf40b75d1b03e44562b5
Felipe Peter <mr-peipei@web.de>
2021-01-04 13:18:19 +0100

    Adjust reward function for antmaze medium diverse

--- benchmark/test/test_reward_functions.py
+++ benchmark/test/test_reward_functions.py
@@ -1,4 +1,4 @@
-from benchmark.utils.reward_functions import antmaze_umaze_diverse_rew_fn, get_reward_function
+from benchmark.utils.reward_functions import antmaze_medium_diverse_rew_fn, antmaze_umaze_diverse_rew_fn, get_reward_function
 import pytest
 import gym
 import d4rl  # noqa
@@ -33,6 +33,24 @@ def test_antmaze_umaze_diverse_reward_function():
 
     assert rew_fn is not None
     assert rew_fn is antmaze_umaze_diverse_rew_fn
+
+    rewards = rew_fn(buffer.obs_buf.unsqueeze(0))
+
+    assert rewards.shape[0] == 1
+    assert rewards.shape[1] == buffer.obs_buf.shape[0]
+    assert rewards.shape[2] == 1
+
+
+@pytest.mark.medium
+def test_antmaze_medium_diverse_reward_function():
+    env_name = 'antmaze-medium-diverse-v0'
+    env = gym.make(env_name)
+    buffer, obs_dim, act_dim = load_dataset_from_env(env, 1000)
+
+    rew_fn = get_reward_function(env_name)
+
+    assert rew_fn is not None
+    assert rew_fn is antmaze_medium_diverse_rew_fn
 
     rewards = rew_fn(buffer.obs_buf.unsqueeze(0))
 
--- benchmark/utils/reward_functions.py
+++ benchmark/utils/reward_functions.py
@@ -1,4 +1,4 @@
-from benchmark.utils.mazes import ANTMAZE_ANT_RADIUS, ANTMAZE_UMAZE_DIVERSE_GOAL, ANTMAZE_UMAZE_GOAL
+from benchmark.utils.mazes import ANTMAZE_ANT_RADIUS, ANTMAZE_MEDIUM_DIVERSE_GOAL, ANTMAZE_UMAZE_DIVERSE_GOAL, ANTMAZE_UMAZE_GOAL
 import torch
 
 
@@ -28,12 +28,12 @@ def antmaze_umaze_diverse_rew_fn(next_obs, **_):
     return rewards
 
 
-def antmaze_medium_rew_fn(next_obs, **_):
+def antmaze_medium_diverse_rew_fn(next_obs, **_):
     x = next_obs[:, :, 0]
     y = next_obs[:, :, 1]
 
-    rewards = torch.sqrt(torch.square(x - ANTMAZE_UMAZE_GOAL[0]) +
-                         torch.square(y - ANTMAZE_UMAZE_GOAL[1])) < ANTMAZE_ANT_RADIUS
+    rewards = torch.sqrt(torch.square(x - ANTMAZE_MEDIUM_DIVERSE_GOAL[0]) +
+                         torch.square(y - ANTMAZE_MEDIUM_DIVERSE_GOAL[1])) < ANTMAZE_ANT_RADIUS
     rewards = rewards.unsqueeze(-1)
 
     rewards = rewards*100
@@ -44,7 +44,7 @@ def antmaze_medium_rew_fn(next_obs, **_):
 reward_functions = {
     'antmaze-umaze-v0': antmaze_umaze_rew_fn,
     'antmaze-umaze-diverse-v0': antmaze_umaze_diverse_rew_fn,
-    'antmaze_medium': antmaze_medium_rew_fn
+    'antmaze-medium-diverse-v0': antmaze_medium_diverse_rew_fn
 }
 
 
