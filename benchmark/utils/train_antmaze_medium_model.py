30279463a0865a4397522bf9a34c9cfd3c44184f
Felipe Peter <mr-peipei@web.de>
2021-01-05 14:16:26 +0100

    Use lr schedule for training antmaze medium model

--- benchmark/utils/train_antmaze_medium_model.py
+++ benchmark/utils/train_antmaze_medium_model.py
@@ -11,7 +11,7 @@ from benchmark.utils.str2bool import str2bool
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--augment', type=str2bool, default=False)
-    parser.add_argument('--rew_amp', type=int, default=100)
+    parser.add_argument('--rew_amp', type=int, default=0.05)
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     model = EnvironmentModel(obs_dim=obs_dim,
                              act_dim=act_dim,
-                             hidden=4*[512],
+                             hidden=5*[512],
                              type='probabilistic',
                              n_networks=15,
                              device=device,
@@ -45,9 +45,9 @@ if __name__ == '__main__':
                                  'antmaze-medium_diverse-v0'),
                              )
 
-    model.train_to_convergence(buffer, debug=True, batch_size=128, lr=5e-4,
+    model.train_to_convergence(buffer, debug=True, batch_size=128,
                                max_n_train_batches=-1, patience=15,
-                               val_split=0.1)
+                               val_split=0.1, lr_schedule=(1e-4, 1e-3))
     model.cpu()
 
     torch.save(model, save_path)
