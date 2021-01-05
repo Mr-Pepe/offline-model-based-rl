ef320c8191d260ef1fe1678619092f970dff5a53
Felipe Peter <mr-peipei@web.de>
2021-01-05 14:24:05 +0100

    Rename to antmaze medium diverse

--- benchmark/utils/train_antmaze_medium_diverse_model.py
+++ benchmark/utils/train_antmaze_medium_diverse_model.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from benchmark.utils.postprocessing import get_postprocessing_function
 from benchmark.utils.preprocessing import get_preprocessing_function
@@ -10,25 +11,15 @@ from benchmark.utils.str2bool import str2bool
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--augment', type=str2bool, default=False)
     parser.add_argument('--rew_amp', type=int, default=0.05)
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     env = gym.make('antmaze-medium-diverse-v0')
 
-    if args.augment:
-        print("Load augmented dataset")
-        buffer = torch.load(
-            '/home/felipe/Projects/thesis-code/data/datasets/antmaze_medium_augmented.p')
-        obs_dim = env.observation_space.shape[0]
-        act_dim = env.action_space.shape[0]
-        save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium/augmented_model.pt'
-    else:
-        print("Load non-augmented dataset")
-        buffer, obs_dim, act_dim = load_dataset_from_env(env,
-                                                         buffer_device=device)
-        save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium/model.pt'
+    buffer, obs_dim, act_dim = load_dataset_from_env(env,
+                                                     buffer_device=device)
+    save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium_diverse/model.pt'
 
     buffer.rew_buf *= args.rew_amp
     print("Amplified reward with factor {}".format(args.rew_amp))
@@ -47,7 +38,9 @@ if __name__ == '__main__':
 
     model.train_to_convergence(buffer, debug=True, batch_size=128,
                                max_n_train_batches=-1, patience=15,
-                               val_split=0.1, lr_schedule=(1e-4, 1e-3))
+                               val_split=0.05, lr_schedule=(1e-4, 1e-3))
     model.cpu()
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
     torch.save(model, save_path)
