9011f96bf0286a7d491cb55f65df5bd9b05e8814
Felipe Peter <mr-peipei@web.de>
2021-01-09 21:53:47 +0100

    Train medium model on reduced dataset

--- benchmark/utils/train_antmaze_medium_diverse_model.py
+++ benchmark/utils/train_antmaze_medium_diverse_model.py
@@ -1,6 +1,7 @@
+from benchmark.utils.replay_buffer import ReplayBuffer
 import os
 import argparse
-from benchmark.utils.postprocessing import get_postprocessing_function
+from benchmark.utils.postprocessing import get_postprocessing_function, postprocess_antmaze_medium
 from benchmark.utils.preprocessing import get_preprocessing_function
 from benchmark.models.environment_model import EnvironmentModel
 from benchmark.utils.load_dataset import load_dataset_from_env
@@ -12,6 +13,7 @@ from benchmark.utils.str2bool import str2bool
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--rew_amp', type=int, default=0.05)
+    parser.add_argument('--reduced', type=str2bool, default=True)
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -23,6 +25,23 @@ if __name__ == '__main__':
 
     buffer.rew_buf *= args.rew_amp
     print("Amplified reward with factor {}".format(args.rew_amp))
+
+    if args.reduced:
+        train_buffer = ReplayBuffer(
+            obs_dim, act_dim, buffer.size, device=device)
+        train_idx = ~postprocess_antmaze_medium(
+            next_obs=buffer.obs2_buf[:buffer.size].unsqueeze(0), ant_radius=0.8)['dones'].view(-1)
+        train_idx = torch.logical_and(train_idx, (buffer.obs2_buf[:, 2] > 0.3))
+
+        train_buffer.store_batch(buffer.obs_buf[train_idx],
+                                 buffer.act_buf[train_idx],
+                                 buffer.rew_buf[train_idx],
+                                 buffer.obs2_buf[train_idx],
+                                 buffer.done_buf[train_idx],)
+
+        buffer = train_buffer
+
+        save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium_diverse/reduced_model.pt'
 
     model = EnvironmentModel(obs_dim=obs_dim,
                              act_dim=act_dim,
