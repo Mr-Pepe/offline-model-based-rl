3c6bf58faab2f63db18b4b0725d36ef2ca8038fb
Felipe Peter <mr-peipei@web.de>
2021-01-14 11:24:57 +0100

    Use augmentation for medium diverse model

--- benchmark/utils/train_antmaze_medium_diverse_model.py
+++ benchmark/utils/train_antmaze_medium_diverse_model.py
@@ -1,3 +1,5 @@
+from benchmark.utils.mazes import ANTMAZE_MEDIUM_MAX, ANTMAZE_MEDIUM_MIN
+from benchmark.utils.augmentation import antmaze_augmentation
 from benchmark.utils.replay_buffer import ReplayBuffer
 import os
 import argparse
@@ -13,8 +15,8 @@ from benchmark.utils.str2bool import str2bool
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--rew_amp', type=int, default=0.05)
-    parser.add_argument('--reduced', type=str2bool, default=False)
-    parser.add_argument('--data_path', type=str, default='/home/felipe/Projects/thesis-code/data/datasets/antmaze_medium_diverse_augmented.p')
+    parser.add_argument('--augmented', type=str2bool, default=True)
+    parser.add_argument('--data_path', type=str, default='')
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -28,7 +30,9 @@ if __name__ == '__main__':
     buffer.rew_buf *= args.rew_amp
     print("Amplified reward with factor {}".format(args.rew_amp))
 
-    if args.reduced:
+    augmentation_fn = None
+
+    if args.augmented:
         train_buffer = ReplayBuffer(
             obs_dim, act_dim, buffer.size, device=device)
         train_idx = ~postprocess_antmaze_medium(
@@ -41,12 +45,15 @@ if __name__ == '__main__':
                                  buffer.obs2_buf[train_idx],
                                  buffer.done_buf[train_idx],)
 
+        def augmentation_fn(x, y): return antmaze_augmentation(x,
+                                                               y,
+                                                               ANTMAZE_MEDIUM_MIN,
+                                                               ANTMAZE_MEDIUM_MAX,
+                                                               ANTMAZE_MEDIUM_MIN,
+                                                               ANTMAZE_MEDIUM_MAX)
+
         buffer = train_buffer
 
-        save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium_diverse/reduced_model.pt'
-
-    if args.data_path != '':
-        buffer = torch.load(args.data_path)
         save_path = '/home/felipe/Projects/thesis-code/data/models/antmaze_medium_diverse/augmented_model.pt'
 
     model = EnvironmentModel(obs_dim=obs_dim,
@@ -64,7 +71,7 @@ if __name__ == '__main__':
     model.train_to_convergence(buffer, debug=True, batch_size=128,
                                max_n_train_batches=-1, patience=2,
                                val_split=0.05, lr_schedule=(1e-4, 1e-3),
-                               no_reward=True)
+                               no_reward=True, augmentation_fn=augmentation_fn)
     model.cpu()
 
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
