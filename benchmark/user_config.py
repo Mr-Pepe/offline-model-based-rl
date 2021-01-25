75ecd2ab3c983e55c7734a896999cd4a60cb24ca
Felipe Peter <mr-peipei@web.de>
2021-01-25 18:21:12 +0100

    Fix user config

--- benchmark/user_config.py
+++ benchmark/user_config.py
@@ -4,8 +4,17 @@ import os.path as osp
 # Where experiment outputs are saved by default:
 DEFAULT_DATA_DIR = osp.join(Path.home(), 'Projects', 'thesis-code', 'data')
 
+MODELS_DIR = osp.join(Path.home(), 'Projects', 'thesis-code', 'benchmark', 'models')
+
+
 # Whether to automatically insert a date and time stamp into the names of
 # save directories:
 FORCE_DATESTAMP = False
 
-MODELS_DIR = osp.join(Path.home(), 'Projects', 'thesis-code', 'benchmark', 'models')
+
+# Whether GridSearch provides automatically-generated default shorthands:
+DEFAULT_SHORTHAND = True
+
+# Tells the GridSearch how many seconds to pause for before launching 
+# experiments.
+WAIT_BEFORE_LAUNCH = 5
