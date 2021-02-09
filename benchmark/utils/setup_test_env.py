5bbab595586377c04d8ed3e7238c61efa4f99e57
Felipe Peter <mr-peipei@web.de>
2021-02-09 14:12:17 +0100

    Fix test env setup

--- benchmark/utils/setup_test_env.py
+++ benchmark/utils/setup_test_env.py
@@ -2,22 +2,23 @@ import numpy as np
 
 
 def setup_test_env(env):
-    if env.spec.id == 'antmaze-medium-diverse-v0':
-        start_state = ANTMAZE_MEDIUM_DIVERSE_START_STATES[np.random.choice(
-            len(ANTMAZE_MEDIUM_DIVERSE_START_STATES), 1)[0]]
-        o = env.reset()
-        o = np.concatenate((start_state, o[2:15], o[15:]))
-        env.set_state(o[:15], o[15:])
-        return o
-    elif env.spec.id == 'antmaze-umaze-v0':
-        start_state = ANTMAZE_UMAZE_START_STATES[np.random.choice(
-            len(ANTMAZE_UMAZE_START_STATES), 1)[0]]
-        o = env.reset()
-        o = np.concatenate((start_state, o[2:15], o[15:]))
-        env.set_state(o[:15], o[15:])
-        return o
-    else:
-        return env.reset()
+    if env.spec is not None:
+        if env.spec.id == 'antmaze-medium-diverse-v0':
+            start_state = ANTMAZE_MEDIUM_DIVERSE_START_STATES[np.random.choice(
+                len(ANTMAZE_MEDIUM_DIVERSE_START_STATES), 1)[0]]
+            o = env.reset()
+            o = np.concatenate((start_state, o[2:15], o[15:]))
+            env.set_state(o[:15], o[15:])
+            return o
+        elif env.spec.id == 'antmaze-umaze-v0':
+            start_state = ANTMAZE_UMAZE_START_STATES[np.random.choice(
+                len(ANTMAZE_UMAZE_START_STATES), 1)[0]]
+            o = env.reset()
+            o = np.concatenate((start_state, o[2:15], o[15:]))
+            env.set_state(o[:15], o[15:])
+            return o
+
+    return env.reset()
 
 
 ANTMAZE_MEDIUM_DIVERSE_START_STATES = [(0, 0),
