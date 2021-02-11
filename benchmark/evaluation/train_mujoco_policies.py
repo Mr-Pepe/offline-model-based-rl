718f4a18fc666f100014fdb325832f62e8dd4ca0
Felipe Peter <mr-peipei@web.de>
2021-02-11 17:43:19 +0100

    Add hopper medium partitioning for tuning

--- benchmark/evaluation/train_mujoco_policies.py
+++ benchmark/evaluation/train_mujoco_policies.py
@@ -1,12 +1,13 @@
 import argparse
-from benchmark.utils.modes import MODES, PARTITIONING_MODES, PENALTY_MODES
+import numpy as np
+from benchmark.utils.modes import ALEATORIC_PARTITIONING, ALEATORIC_PENALTY, EPISTEMIC_PARTITIONING, EXPLICIT_PARTITIONING, MODES, PARTITIONING_MODES, PENALTY_MODES
 from benchmark.utils.run_utils import setup_logger_kwargs
 from ray import tune
 import ray
 from ray.tune.schedulers.async_hyperband import ASHAScheduler
 from ray.tune.suggest.hyperopt import HyperOptSearch
 from benchmark.utils.envs import \
-    HALF_CHEETAH_MEDIUM_EXPERT, HALF_CHEETAH_MEDIUM_REPLAY, \
+    HALF_CHEETAH_MEDIUM_EXPERT, HALF_CHEETAH_MEDIUM_REPLAY, HOPPER_MEDIUM, \
     HOPPER_MEDIUM_EXPERT, WALKER_MEDIUM_EXPERT
 from benchmark.user_config import MODELS_DIR
 from benchmark.train import Trainer
@@ -27,7 +28,7 @@ if __name__ == '__main__':
     parser.add_argument('--env_name', type=str,
                         default=HALF_CHEETAH_MEDIUM_REPLAY)
     parser.add_argument('--level', type=int, default=0)
-    parser.add_argument('--mode', type=str, default='mopo')
+    parser.add_argument('--mode', type=str, default=ALEATORIC_PENALTY)
     parser.add_argument('--epochs', type=int, default=200)
     parser.add_argument('--seeds', type=int, default=1)
     parser.add_argument('--pessimism', type=float, default=1)
@@ -86,7 +87,7 @@ if __name__ == '__main__':
         continuous_rollouts=True,
         train_model_every=0,
         use_custom_reward=False,
-        real_buffer_size=int(1e6),
+        real_buffer_size=int(2e6),
         virtual_buffer_size=int(1e6),
         reset_buffer=False,
         virtual_pretrain_epochs=0,
@@ -116,11 +117,6 @@ if __name__ == '__main__':
             ood_threshold=args.ood_threshold,
             pretrained_agent_path=args.pretrained_agent_path
         )
-
-        if args.env_name in [HALF_CHEETAH_MEDIUM_EXPERT,
-                             HOPPER_MEDIUM_EXPERT,
-                             WALKER_MEDIUM_EXPERT]:
-            config.update(real_buffer_size=int(2e6))
 
         assert config['sac_kwargs']['batch_size'] is not None
         assert config['sac_kwargs']['agent_hidden'] is not None
@@ -155,19 +151,40 @@ if __name__ == '__main__':
         if args.level == 1:
             config.update(
                 epochs=30,
-                sac_kwargs=dict(batch_size=tune.choice([128, 256, 512]),
-                                agent_hidden=tune.choice([32, 64, 128, 256]),
-                                gamma=tune.uniform(0.99, 0.999),
-                                pi_lr=tune.loguniform(1e-5, 1e-2),
-                                q_lr=tune.loguniform(1e-5, 1e-2),
+                sac_kwargs=dict(batch_size=256,
+                                agent_hidden=128,
+                                gamma=0.99,
+                                pi_lr=3e-4,
+                                q_lr=3e-4,
                                 ),
-                rollouts_per_step=tune.randint(1, 401),
-                max_rollout_length=tune.randint(1, 50),
-                model_pessimism=tune.uniform(0.001, 1000)
             )
 
-            if args.env_name == HALF_CHEETAH_MEDIUM_EXPERT:
-                config.update(real_buffer_size=int(2e6))
+            if args.mode in PARTITIONING_MODES:
+
+                if args.env_name == HOPPER_MEDIUM:
+
+                    if args.mode == ALEATORIC_PARTITIONING:
+                        max_value = 2.4976
+                        mean_value = 0.1656
+                        std_value = 0.4030
+
+                    if args.mode == EPISTEMIC_PARTITIONING:
+                        max_value = 7.9778
+                        mean_value = 0.3031
+                        std_value = 0.4823
+
+                    if args.mode == EXPLICIT_PARTITIONING:
+                        max_value = 2.4976
+                        mean_value = 0.1656
+                        std_value = 0.4030
+
+                config.update(
+                    rollouts_per_step=tune.randint(1, 100),
+                    max_rollout_length=tune.randint(1, 50),
+                    ood_threshold=tune.grid_search(
+                        [mean_value + x*std_value for x in np.linspace(0, (max_value-mean_value)/std_value, 5)]),
+                    model_pessimism=0
+                )
 
         assert config['sac_kwargs']['batch_size'] is not None
         assert config['sac_kwargs']['agent_hidden'] is not None
@@ -181,10 +198,10 @@ if __name__ == '__main__':
 
         ray.init()
         scheduler = ASHAScheduler(
-            time_attr='time_since_restore',
+            time_attr='training_iteration',
             metric='avg_test_return',
             mode='max',
-            max_t=1000,
+            max_t=30,
             grace_period=10,
             reduction_factor=3,
             brackets=1)
@@ -199,7 +216,7 @@ if __name__ == '__main__':
             '-tuning-lvl-'+str(args.level),
             scheduler=scheduler,
             search_alg=search_alg,
-            num_samples=200,
+            num_samples=5,
             config=config,
             resources_per_trial={"gpu": 0.5}
         )
