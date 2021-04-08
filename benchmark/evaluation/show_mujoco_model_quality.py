7518a9cbb859dae9d95ea05e7d59c5b6eb5ba161
Felipe Peter <mr-peipei@web.de>
2021-04-08 09:02:57 +0200

    Make model quality plots prettier

--- benchmark/evaluation/show_mujoco_model_quality.py
+++ benchmark/evaluation/show_mujoco_model_quality.py
@@ -26,7 +26,7 @@ dataset_names = [
     'expert',
 ]
 
-fig, axes = plt.subplots(2, len(dataset_names))
+fig, axes = plt.subplots(2, len(dataset_names), figsize=(8, 5))
 
 fig.suptitle(name, fontsize=24)
 
@@ -58,12 +58,15 @@ for i_dataset, dataset_name in enumerate(dataset_names):
     while i_sample < len(obs_buf):
         print(i_sample)
         i_to = min(len(obs_buf), i_sample + batch_size)
-        obs_act = torch.cat((obs_buf[i_sample:i_to], act_buf[i_sample:i_to]), dim=1)
+        obs_act = torch.cat(
+            (obs_buf[i_sample:i_to], act_buf[i_sample:i_to]), dim=1)
         obs2 = obs2_buf[i_sample:i_to]
 
-        prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(obs_act, debug=True)
+        prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(
+            obs_act, debug=True)
 
-        model_errors.extend((prediction[:, :-2] - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
+        model_errors.extend(
+            (prediction[:, :-2] - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
         raw_aleatoric_uncertainties.extend(aleatoric_uncertainty.tolist())
         raw_epistemic_uncertainties.extend(epistemic_uncertainty.tolist())
 
@@ -76,42 +79,72 @@ for i_dataset, dataset_name in enumerate(dataset_names):
 
         for i_obs in range(random_samples):
             obs = obs_buf[i_obs]
-            env.set_state(torch.cat((torch.as_tensor([0]), torch.as_tensor(obs[:env.model.nq-1].cpu()))), obs[env.model.nq-1:].cpu())
+            env.set_state(torch.cat((torch.as_tensor([0]), torch.as_tensor(
+                obs[:env.model.nq-1].cpu()))), obs[env.model.nq-1:].cpu())
             act = agent.act()
 
             obs2, _, _, _ = env.step(act.cpu().detach().numpy())
 
             obs_act = torch.cat((obs.unsqueeze(0), act), dim=1)
 
-            prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(obs_act, debug=True)
+            prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(
+                obs_act, debug=True)
 
-            model_errors.extend((prediction[:, :-2].cpu() - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
+            model_errors.extend(
+                (prediction[:, :-2].cpu() - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
             raw_aleatoric_uncertainties.extend(aleatoric_uncertainty.tolist())
             raw_epistemic_uncertainties.extend(epistemic_uncertainty.tolist())
 
     raw_aleatoric_uncertainties = torch.as_tensor(raw_aleatoric_uncertainties)
-    aleatoric_uncertainties = raw_aleatoric_uncertainties / raw_aleatoric_uncertainties.max()
+    aleatoric_uncertainties = raw_aleatoric_uncertainties / \
+        raw_aleatoric_uncertainties.max()
     raw_epistemic_uncertainties = torch.as_tensor(raw_epistemic_uncertainties)
-    epistemic_uncertainties = raw_epistemic_uncertainties / raw_epistemic_uncertainties.max()
+    epistemic_uncertainties = raw_epistemic_uncertainties / \
+        raw_epistemic_uncertainties.max()
     model_errors = torch.as_tensor(model_errors)
     model_errors /= model_errors.max()
 
-    combined_uncertainties = torch.sqrt((raw_aleatoric_uncertainties + 1).square() - 1) + torch.sqrt((raw_epistemic_uncertainties + 1).square() - 1)
+    combined_uncertainties = torch.sqrt((raw_aleatoric_uncertainties + 1).square(
+    ) - 1) + torch.sqrt((raw_epistemic_uncertainties + 1).square() - 1)
     combined_uncertainties /= combined_uncertainties.max()
 
-    axes[0, i_dataset].plot([0, 1], [0, 1], color='black')
-    axes[0, i_dataset].scatter(model_errors[:n_samples], aleatoric_uncertainties[:n_samples], s=1, alpha=0.3, color='blue')
-    axes[0, i_dataset].scatter(model_errors[n_samples:], aleatoric_uncertainties[n_samples:], s=1, alpha=0.3, color='red')
+    axes[0, i_dataset].plot([-1, 2], [-1, 2], color='black')
+    axes[0, i_dataset].scatter(
+        model_errors[:n_samples], aleatoric_uncertainties[:n_samples], s=1, alpha=0.3, color='blue')
+    axes[0, i_dataset].scatter(
+        model_errors[n_samples:], aleatoric_uncertainties[n_samples:], s=1, alpha=0.3, color='red')
     if i_dataset == 0:
-        axes[0, i_dataset].set_ylabel('Aleatoric uncertainty')
-    axes[0, i_dataset].set_title(dataset_name)
+        axes[0, i_dataset].set_ylabel('Aleatoric uncertainty', fontsize=12)
+        axes[0, i_dataset].set_yticklabels([0, 0.2, 0.4, 0.6, 0.8, 1])
+    else:
+        axes[0, i_dataset].set_yticklabels([])
+        for tic in axes[0, i_dataset].yaxis.get_major_ticks():
+            tic.tick1line.set_visible(False)
+            tic.tick2line.set_visible(False)
 
-    axes[1, i_dataset].plot([0, 1], [0, 1], color='black')
-    axes[1, i_dataset].scatter(model_errors[:n_samples], epistemic_uncertainties[:n_samples], s=1, alpha=0.3, color='blue')
-    axes[1, i_dataset].scatter(model_errors[n_samples:], epistemic_uncertainties[n_samples:], s=1, alpha=0.3, color='red')
+    for tic in axes[0, i_dataset].xaxis.get_major_ticks():
+        tic.tick1line.set_visible(False)
+        tic.tick2line.set_visible(False)
+    axes[0, i_dataset].set_xticklabels([])
+    axes[0, i_dataset].set_title(dataset_name)
+    axes[0, i_dataset].set_xlim([0, 1])
+    axes[0, i_dataset].set_ylim([0, 1])
+
+    axes[1, i_dataset].plot([-1, 2], [-1, 2], color='black')
+    axes[1, i_dataset].scatter(
+        model_errors[:n_samples], epistemic_uncertainties[:n_samples], s=1, alpha=0.3, color='blue')
+    axes[1, i_dataset].scatter(
+        model_errors[n_samples:], epistemic_uncertainties[n_samples:], s=1, alpha=0.3, color='red')
     axes[1, i_dataset].set_xlabel('Model error')
     if i_dataset == 0:
-        axes[1, i_dataset].set_ylabel('Epistemic uncertainty')
+        axes[1, i_dataset].set_ylabel('Epistemic uncertainty', fontsize=12)
+    else:
+        axes[1, i_dataset].set_yticklabels([])
+        for tic in axes[1, i_dataset].yaxis.get_major_ticks():
+            tic.tick1line.set_visible(False)
+            tic.tick2line.set_visible(False)
+    axes[1, i_dataset].set_xlim([0, 1])
+    axes[1, i_dataset].set_ylim([0, 1])
 
     # axes[2, i_dataset].plot([0, 1], [0, 1], color='black')
     # axes[2, i_dataset].scatter(model_errors[:n_samples], combined_uncertainties[:n_samples], s=1, alpha=0.3, color='blue')
@@ -119,5 +152,16 @@ for i_dataset, dataset_name in enumerate(dataset_names):
     # axes[2, i_dataset].set_xlabel('Model error')
     # if i_dataset == 0:
     #     axes[2, i_dataset].set_ylabel('Combined uncertainty')
+
+for ax in axes.flat:
+    ax.grid(b=True, alpha=0.5, linestyle="--")
+
+fig.subplots_adjust(top=0.85,
+                    bottom=0.085,
+                    left=0.07,
+                    right=0.985,
+                    hspace=0.12,
+                    wspace=0.2)
+
 
 plt.show()
