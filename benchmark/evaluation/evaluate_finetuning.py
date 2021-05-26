74fce4108bcb70b565807aed46d3456b1a64e356
Felipe Peter <mr-peipei@web.de>
2021-05-26 12:02:24 +0200

    Nicer finetuning evaluation

--- benchmark/evaluation/evaluate_finetuning.py
+++ benchmark/evaluation/evaluate_finetuning.py
@@ -1,5 +1,4 @@
-from benchmark.utils.modes import ALEATORIC_PARTITIONING, ALEATORIC_PENALTY, BEHAVIORAL_CLONING, EPISTEMIC_PARTITIONING, EPISTEMIC_PENALTY, MBPO, SAC
-from benchmark.utils.envs import HALF_CHEETAH_ENVS, HALF_CHEETAH_MEDIUM_V2, HOPPER_ENVS, HOPPER_MEDIUM_EXPERT_V2, HOPPER_MEDIUM_REPLAY_V2, HOPPER_MEDIUM_V2, WALKER_ENVS, WALKER_MEDIUM_EXPERT_V2, WALKER_MEDIUM_REPLAY_V2, WALKER_MEDIUM_v2
+import d4rl
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,28 +11,29 @@ from benchmark.utils.env_name_from_exp_name import get_env_name
 from benchmark.utils.str2bool import str2bool
 from d4rl import get_normalized_score
 import torch
-from benchmark.evaluation.plot import make_plots
+from benchmark.evaluation.plot import get_all_datasets, make_plots, plot_data
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('logdir', default=[
-                        '/home/felipe/Projects/thesis-code/data/online_experiments/aleatoric_penalty',
-                        '/home/felipe/Projects/thesis-code/data/online_experiments/aleatoric-partitioning',
-                        '/home/felipe/Projects/thesis-code/data/online_experiments/epistemic-penalty/walker2d-medium-v2-sac-pretrained-50000samples',
-                        '/home/felipe/Projects/thesis-code/data/online_experiments/epistemic-partitioning/walker2d-medium-v2-sac-pretrained-50000samples'
-                        ], nargs='*')
-    parser.add_argument('--legend', '-l', nargs='*')
-    parser.add_argument('--xaxis', '-x', default='Epoch')
-    parser.add_argument(
-        '--value', '-y', default=['AverageTestEpRet'], nargs='*')
-    parser.add_argument('--count', action='store_true')
-    parser.add_argument('--smooth', '-s', type=int, default=15)
-    parser.add_argument('--select', nargs='*')
-    parser.add_argument('--exclude', nargs='*')
-    parser.add_argument('--est', default='mean')
-    args = parser.parse_args()
+    logdir = [
+        ['/home/felipe/Projects/thesis-code/data/finetuning/halfcheetah/aleatoric-penalty/halfcheetah-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/halfcheetah/aleatoric-partitioning/halfcheetah-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/halfcheetah/epistemic-penalty/halfcheetah-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/halfcheetah/epistemic-partitioning/halfcheetah-medium-v2-sac-pretrained-50000samples'],
+        ['/home/felipe/Projects/thesis-code/data/finetuning/hopper/aleatoric-penalty/hopper-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/hopper/aleatoric-partitioning/hopper-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/hopper/epistemic-penalty/hopper-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/hopper/epistemic-partitioning/hopper-medium-v2-sac-pretrained-50000samples', ],
+        ['/home/felipe/Projects/thesis-code/data/finetuning/walker2d/aleatoric-penalty/walker2d-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/walker2d/aleatoric-partitioning/walker2d-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/walker2d/epistemic-penalty/walker2d-medium-v2-sac-pretrained-50000samples',
+         '/home/felipe/Projects/thesis-code/data/finetuning/walker2d/epistemic-partitioning/walker2d-medium-v2-sac-pretrained-50000samples', ],
+    ]
+
+    env_names = ['halfcheetah-medium-v2',
+                 'hopper-medium-v2',
+                 'walker2d-medium-v2',
+                 ]
 
     legend = [
         'aleatoric penalty',
@@ -42,6 +42,35 @@ if __name__ == "__main__":
         'epistemic partitioning'
     ]
 
-    make_plots(args.logdir, legend, args.xaxis, args.value, args.count,
-               smooth=args.smooth, select=args.select, exclude=args.exclude,
-               estimator=args.est)
+    f, axes = plt.subplots(1, 3, figsize=(8, 4))
+
+    for i_axis, ax in enumerate(axes):
+        def normalize_score(x):
+            return d4rl.get_normalized_score(env_names[i_axis], x)*100
+
+        plt.sca(ax)
+        data = get_all_datasets(logdir[i_axis], legend=legend)
+        for datum in data:
+            datum['AverageTestEpRet'] = [normalize_score(
+                x) for x in datum['AverageTestEpRet']]
+        condition = 'Condition1'
+        estimator = getattr(np, 'mean')
+        plot_data(data, xaxis='Epoch', value='AverageTestEpRet', condition=condition,
+                  smooth=15, estimator=estimator)
+        ax.grid(b=True, alpha=0.5, linestyle="--")
+        ax.get_legend().remove()
+        ax.set_ylabel('Performance')
+
+    if axes[0].get_legend_handles_labels()[1] != axes[1].get_legend_handles_labels()[1] or axes[0].get_legend_handles_labels()[1] != axes[2].get_legend_handles_labels()[1]:
+        raise AssertionError("Legend handles not identical")
+
+    f.legend(*axes[0].get_legend_handles_labels(),
+             loc='lower center', prop={'size': 12}, ncol=2)
+
+    f.subplots_adjust(top=0.969,
+                      bottom=0.274,
+                      left=0.086,
+                      right=0.977,
+                      hspace=0.2,
+                      wspace=0.396)
+    plt.show()
