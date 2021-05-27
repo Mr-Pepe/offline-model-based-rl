ce0a3bd284ef09ba1e918520a7786ee4ccca08b4
Felipe Peter <mr-peipei@web.de>
2021-05-27 16:10:07 +0200

    Fix latex generation

--- benchmark/evaluation/evaluate_model_metrics.py
+++ benchmark/evaluation/evaluate_model_metrics.py
@@ -26,112 +26,117 @@ dataset_names = [
     'expert-v2',
 ]
 
+compute = True
+
 results = dict()
+if compute:
+    for env_title in env_names:
 
-for env_title in env_names:
+        for i_dataset, dataset_name in enumerate(dataset_names):
+            device = 'cuda'
+            env_name = env_title + f'-{dataset_name}'
+            print(env_name)
+            env = gym.make(env_name)
 
-    for i_dataset, dataset_name in enumerate(dataset_names):
-        device = 'cuda'
-        env_name = env_title + f'-{dataset_name}'
-        print(env_name)
-        env = gym.make(env_name)
+            n_samples = 10000
 
-        n_samples = 10000
+            buffer, obs_dim, act_dim = load_dataset_from_env(env, n_samples)
 
-        buffer, obs_dim, act_dim = load_dataset_from_env(env, n_samples)
+            model = torch.load(os.path.join(
+                MODELS_DIR, env_name + '-model.pt'))
 
-        model = torch.load(os.path.join(MODELS_DIR, env_name + '-model.pt'))
+            random_samples = 10000
+            i_sample = 0
+            batch_size = 1024
 
-        random_samples = 10000
-        i_sample = 0
-        batch_size = 1024
+            idxs = torch.randint(0, buffer.size, (n_samples,))
+            obs_buf = buffer.obs_buf[idxs].cuda()
+            act_buf = buffer.act_buf[idxs].cuda()
+            obs2_buf = buffer.obs2_buf[idxs].cuda()
 
-        idxs = torch.randint(0, buffer.size, (n_samples,))
-        obs_buf = buffer.obs_buf[idxs].cuda()
-        act_buf = buffer.act_buf[idxs].cuda()
-        obs2_buf = buffer.obs2_buf[idxs].cuda()
+            model_errors = []
+            aleatoric_uncertainties = []
+            epistemic_uncertainties = []
 
-        model_errors = []
-        aleatoric_uncertainties = []
-        epistemic_uncertainties = []
-
-        while i_sample < len(obs_buf):
-            print(f'ID: {i_sample}/{len(obs_buf)}', end='\r')
-            i_to = min(len(obs_buf), i_sample + batch_size)
-            obs_act = torch.cat(
-                (obs_buf[i_sample:i_to], act_buf[i_sample:i_to]), dim=1)
-            obs2 = obs2_buf[i_sample:i_to]
-
-            prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(
-                obs_act, debug=True)
-
-            model_errors.extend(
-                (prediction[:, :-2] - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
-            aleatoric_uncertainties.extend(aleatoric_uncertainty.tolist())
-            epistemic_uncertainties.extend(epistemic_uncertainty.tolist())
-
-            i_sample += batch_size
-
-        aleatoric_uncertainties = torch.as_tensor(aleatoric_uncertainties)
-        epistemic_uncertainties = torch.as_tensor(epistemic_uncertainties)
-        model_errors = torch.as_tensor(model_errors)
-
-        al_corr = estimation_quality(model_errors, aleatoric_uncertainties)
-        ep_corr = estimation_quality(model_errors, epistemic_uncertainties)
-        al_over = pessimism(model_errors, aleatoric_uncertainties)
-        ep_over = pessimism(model_errors, epistemic_uncertainties)
-        print(
-            f'ID  (al/ep) (al/ep): {al_corr:.2f}/{ep_corr:.2f}  {al_over:.2f}/{ep_over:.2f}')
-
-        results[(env_name, dataset_name, 'ID')] = (
-            al_corr, ep_corr, al_over, ep_over)
-
-        model_errors = []
-        aleatoric_uncertainties = []
-        epistemic_uncertainties = []
-
-        if random_samples > 0:
-            env.reset()
-            agent = RandomAgent(env, device=device)
-
-            for i_obs in range(random_samples):
-                print(f'OOD: {i_obs}/{random_samples}', end='\r')
-                obs = obs_buf[i_obs]
-                env.set_state(torch.cat((torch.as_tensor([0]), torch.as_tensor(
-                    obs[:env.model.nq-1].cpu()))), obs[env.model.nq-1:].cpu())
-                act = agent.act()
-
-                obs2, _, _, _ = env.step(act.cpu().detach().numpy())
-
-                obs_act = torch.cat((obs.unsqueeze(0), act), dim=1)
+            while i_sample < len(obs_buf):
+                print(f'ID: {i_sample}/{len(obs_buf)}', end='\r')
+                i_to = min(len(obs_buf), i_sample + batch_size)
+                obs_act = torch.cat(
+                    (obs_buf[i_sample:i_to], act_buf[i_sample:i_to]), dim=1)
+                obs2 = obs2_buf[i_sample:i_to]
 
                 prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(
                     obs_act, debug=True)
 
                 model_errors.extend(
-                    (prediction[:, :-2].cpu() - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
+                    (prediction[:, :-2] - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
                 aleatoric_uncertainties.extend(aleatoric_uncertainty.tolist())
                 epistemic_uncertainties.extend(epistemic_uncertainty.tolist())
 
-        aleatoric_uncertainties = torch.as_tensor(aleatoric_uncertainties)
-        epistemic_uncertainties = torch.as_tensor(epistemic_uncertainties)
-        model_errors = torch.as_tensor(model_errors)
+                i_sample += batch_size
 
-        al_corr = estimation_quality(model_errors, aleatoric_uncertainties)
-        ep_corr = estimation_quality(model_errors, epistemic_uncertainties)
-        al_over = pessimism(model_errors, aleatoric_uncertainties)
-        ep_over = pessimism(model_errors, epistemic_uncertainties)
-        print(
-            f'OOD  (al/ep) (al/ep): {al_corr:.2f}/{ep_corr:.2f}  {al_over:.2f}/{ep_over:.2f}')
+            aleatoric_uncertainties = torch.as_tensor(aleatoric_uncertainties)
+            epistemic_uncertainties = torch.as_tensor(epistemic_uncertainties)
+            model_errors = torch.as_tensor(model_errors)
 
-        results[(env_name, dataset_name, 'OOD')] = (
-            al_corr, ep_corr, al_over, ep_over)
-        del env
-        del buffer
-        del model
+            al_corr = estimation_quality(model_errors, aleatoric_uncertainties)
+            ep_corr = estimation_quality(model_errors, epistemic_uncertainties)
+            al_over = pessimism(model_errors, aleatoric_uncertainties)
+            ep_over = pessimism(model_errors, epistemic_uncertainties)
+            print(
+                f'ID  (al/ep) (al/ep): {al_corr:.2f}/{ep_corr:.2f}  {al_over:.2f}/{ep_over:.2f}')
 
-with open('model-quality.p', 'wb') as f:
-    pickle.dump(results, f)
+            results[(env_name, dataset_name, 'ID')] = (
+                al_corr, ep_corr, al_over, ep_over)
+
+            model_errors = []
+            aleatoric_uncertainties = []
+            epistemic_uncertainties = []
+
+            if random_samples > 0:
+                env.reset()
+                agent = RandomAgent(env, device=device)
+
+                for i_obs in range(random_samples):
+                    print(f'OOD: {i_obs}/{random_samples}', end='\r')
+                    obs = obs_buf[i_obs]
+                    env.set_state(torch.cat((torch.as_tensor([0]), torch.as_tensor(
+                        obs[:env.model.nq-1].cpu()))), obs[env.model.nq-1:].cpu())
+                    act = agent.act()
+
+                    obs2, _, _, _ = env.step(act.cpu().detach().numpy())
+
+                    obs_act = torch.cat((obs.unsqueeze(0), act), dim=1)
+
+                    prediction, means, logvars, explicit_uncertainty, epistemic_uncertainty, aleatoric_uncertainty, underestimated_reward = model.get_prediction(
+                        obs_act, debug=True)
+
+                    model_errors.extend(
+                        (prediction[:, :-2].cpu() - obs2).abs().mean(dim=1).cpu().detach().float().tolist())
+                    aleatoric_uncertainties.extend(
+                        aleatoric_uncertainty.tolist())
+                    epistemic_uncertainties.extend(
+                        epistemic_uncertainty.tolist())
+
+            aleatoric_uncertainties = torch.as_tensor(aleatoric_uncertainties)
+            epistemic_uncertainties = torch.as_tensor(epistemic_uncertainties)
+            model_errors = torch.as_tensor(model_errors)
+
+            al_corr = estimation_quality(model_errors, aleatoric_uncertainties)
+            ep_corr = estimation_quality(model_errors, epistemic_uncertainties)
+            al_over = pessimism(model_errors, aleatoric_uncertainties)
+            ep_over = pessimism(model_errors, epistemic_uncertainties)
+            print(
+                f'OOD  (al/ep) (al/ep): {al_corr:.2f}/{ep_corr:.2f}  {al_over:.2f}/{ep_over:.2f}')
+
+            results[(env_name, dataset_name, 'OOD')] = (
+                al_corr, ep_corr, al_over, ep_over)
+            del env
+            del buffer
+            del model
+
+    with open('model-quality.p', 'wb') as f:
+        pickle.dump(results, f)
 
 with open('model-quality.p', 'rb') as f:
     results = pickle.load(f)
@@ -147,20 +152,23 @@ for env_title in env_names:
         result_ood = results[(env_name, dataset_name, 'OOD')]
 
         # MSE ID Ep
-        strings[0].append(result_id[1])
+        strings[0].append(f'{result_id[1].item():.2f}')
         # MSE ID Al
-        strings[1].append(result_id[0])
+        strings[1].append(f'{result_id[0].item():.2f}')
         # MSE OOD Ep
-        strings[2].append(result_ood[1])
+        strings[2].append(f'{result_ood[1].item():.2f}')
         # MSE OOD Al
-        strings[3].append(result_ood[0])
+        strings[3].append(f'{result_ood[0].item():.2f}')
         # PS ID Ep
-        strings[4].append(result_id[3])
+        strings[4].append(f'{result_id[3].item():.2f}')
         # PS ID Al
-        strings[5].append(result_id[2])
+        strings[5].append(f'{result_id[2].item():.2f}')
         # PS OOD Ep
-        strings[6].append(result_ood[3])
+        strings[6].append(f'{result_ood[3].item():.2f}')
         # PS OOD Al
-        strings[7].append(result_ood[2])
+        strings[7].append(f'{result_ood[2].item():.2f}')
 
-latex = ' \\\\ '.join([' & '.join(x) for x in strings])
+for x in [' & '.join(x) for x in strings]:
+    print(x)
+# latex = ' \\\\ '.join([' & '.join(x) for x in strings])
+# print(latex)
