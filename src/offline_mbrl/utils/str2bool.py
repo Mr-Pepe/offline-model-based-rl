94765240749de14d0749d2a23474b1ed1109e947
Felipe Peter <mr-peipei@web.de>
2022-09-03 08:24:56 +0800

    Clean up str2bool

--- src/offline_mbrl/utils/str2bool.py
+++ src/offline_mbrl/utils/str2bool.py
@@ -1,14 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Contains a function to convert boolean string values to their boolean value."""
+
 import argparse
 
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
+def str2bool(values: str) -> bool:
+    """Converts a string to a boolean value.
 
-    if v.lower() in ("yes", "true", "t", "y", "1"):
+    Args:
+        value (str): The string value.
+
+    Raises:
+        argparse.ArgumentTypeError: If the string value was not recognized as
+            representing a boolean.
+
+    Returns:
+        bool: The resulting boolean value.
+    """
+    if isinstance(values, bool):
+        return values
+
+    if values.lower() in ("yes", "true", "t", "y", "1"):
         return True
 
-    if v.lower() in ("no", "false", "f", "n", "0"):
+    if values.lower() in ("no", "false", "f", "n", "0"):
         return False
 
     raise argparse.ArgumentTypeError("Boolean value expected.")
