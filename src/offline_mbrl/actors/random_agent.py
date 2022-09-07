fb986028d40a90d8b97ec93c7dc8793739f51314
Felipe Peter <mr-peipei@web.de>
2022-09-07 09:00:38 +0800

    Refactor random agent

--- src/offline_mbrl/actors/random_agent.py
+++ src/offline_mbrl/actors/random_agent.py
@@ -1,16 +1,20 @@
+from typing import Optional
+
+import gym
 import torch
 
 
 class RandomAgent:
-    def __init__(self, env, device="cpu"):
+    def __init__(self, env: gym.Env, device: str = "cpu") -> None:
         self.training = False
         self.act_space = env.action_space
         self.device = device
 
-    def eval(self):
+    def eval(self) -> None:
+        """Dummy function."""
         pass
 
-    def act(self, unused_obs=0):
+    def act(self, unused_observation: Optional[torch.Tensor] = None) -> torch.Tensor:
         return torch.as_tensor(
-            self.act_space.sample().reshape((1, -1)), device=self.device
+            self.act_space.sample().reshape((1, -1)), device=torch.device(self.device)
         )
